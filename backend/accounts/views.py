3fbb9d0a5d3d3edb26dc6892ab93a783bdf17001
mohscode <mohscode@gmail.com>
2025-06-23 04:08:13 +0400

    Initial commit

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -8,7 +8,21 @@ from .serializers import (
     UserSerializer, RoleSerializer, LoginSerializer, 
     ChangePasswordSerializer, PermissionSerializer
 )
+from rest_framework import permissions
 
+# Custom permission class
+class HasPermission(permissions.BasePermission):
+    def __init__(self, permission_codename):
+        self.permission_codename = permission_codename
+
+    def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
+        
+        if request.user.is_superuser:
+            return True
+        
+        return request.user.has_perm(self.permission_codename)
 
 class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
@@ -101,18 +115,3 @@ def toggle_user_status(request, pk):
         return Response(UserSerializer(user).data)
     except User.DoesNotExist:
         return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-
-
-# Custom permission class
-class HasPermission(permissions.BasePermission):
-    def __init__(self, permission_codename):
-        self.permission_codename = permission_codename
-
-    def has_permission(self, request, view):
-        if not request.user or not request.user.is_authenticated:
-            return False
-        
-        if request.user.is_superuser:
-            return True
-        
-        return request.user.has_perm(self.permission_codename)
